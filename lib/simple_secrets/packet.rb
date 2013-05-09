b85c47fdcadbd52173c0b34456b73aa879bb527c
Jay Wagnon <jay.wagnon@gmail.com>
2013-05-09 13:28:01 -0600

    Added build_body and body_to_data methods.

--- lib/simple_secrets/packet.rb
+++ lib/simple_secrets/packet.rb
@@ -10,5 +10,27 @@ module SimpleSecrets
       @master_key = master_key.encode 'BINARY'
       @identity = Primitives.identify @master_key
     end
+
+    def build_body data
+      nonce = Primitives.nonce
+      bindata = Primitives.serialize data
+
+      body = "".encode 'BINARY'
+      body << nonce
+      body << bindata
+
+      Primitives.zero nonce, bindata
+      body
+    end
+
+    def body_to_data body
+      nonce = body[0...16]
+      bindata = body[16..-1]
+
+      data = Primitives.deserialize bindata
+
+      Primitives.zero nonce, bindata
+      data
+    end
   end
 end
--- spec/packet_spec.rb
+++ spec/packet_spec.rb
@@ -5,7 +5,10 @@ include SimpleSecrets
 describe Packet do
 
   let(:master_key){ 'e33f2a7d4f03568cfa955b3316a345653a9d6f0afbb9f66cc10f906f8091e317' }
+  let(:data){ 'foobar' }
+  let(:nonce){ "\r\x82~\x14v\e\xC8\x8F\xD1;\fB\xE3)6\xEC".encode 'BINARY' }  # Generated with Primitives.nonce
 
+  let(:test_body){ "\r\x82~\x14v\e\xC8\x8F\xD1;\fB\xE3)6\xEC\xA6foobar".encode 'BINARY' }
 
   subject{ Packet.new master_key }
 
@@ -19,6 +22,19 @@ describe Packet do
     it 'sets its identity' do
       identity = Primitives.identify master_key.encode 'BINARY'
       subject.instance_variable_get(:@identity).should eq identity
+    end
+  end
+
+  describe '#build_body' do
+    it 'concatenates the serialized body with a nonce' do
+      Primitives.should_receive(:nonce){ nonce }
+      subject.build_body(data).should eq test_body
+    end
+  end
+
+  describe '#body_to_data' do
+    it 'it splits out the nonce and deserializes the body' do
+      subject.body_to_data(test_body).should eq data
     end
   end
 end
